4d51456a1abb23cae1be5290994ca432f426246e
Naginipython <naginipython332@gmail.com>
2024-06-02 18:19:54 -0400

    removed temporary hello test

--- build/src/routes/+page.svelte
+++ build/src/routes/+page.svelte
@@ -4,7 +4,6 @@
     // On mount, prep store and send to set page
     onMount(() => {
         window.location.href = '/library';
-        // window.location.href = '/hello/ben/en';
         // window.location.href = '/browse';
         // window.location.href = '/manga/0/reader/6691898e-fd28-4a82-9f47-86eb77fcefd6';
         // window.location.href = '/manga/a1c7c817-4e59-43b7-9365-09675a149a6f'
--- build/src/routes/hello/[name]/[lang]/+page.js
+++
@@ -1,6 +0,0 @@
-export function load({ params }) {
-    return {
-        name: params.name,
-        lang: params.lang,
-    }
-}
