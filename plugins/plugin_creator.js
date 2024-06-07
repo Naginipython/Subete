d59a6344414ed6a46c35406459ea3bcb0c765b24
Naginipython <naginipython332@gmail.com>
2024-06-07 10:48:44 -0400

    Updated plugin creator, some chrome changes that may not work firefox, and started lib fix for width

--- plugins/plugin_creator.js
+++ plugins/plugin_creator.js
@@ -94,13 +94,64 @@ result.get_pages = "function getChapterPages(json) { let data = []; return data;
 
 // Removes /n, extra spaces, and '\' needed for js things
 for (const [key, val] of Object.entries(result)) {
-  console.log(val);
   result[key] = val.replaceAll('\n', '').replace(/\s+/g, ' ');
 }
-// Writes to a file, to be inserted as a plugin
-fs.writeFile(`${result.id}.json`, JSON.stringify(result), (error) => {
-    if (error) {
-      console.error(error);
-      throw error;
-    }
-});
+
+async function tests() {
+  // Testing if search works
+  const search_res = await fetch(result.search_url.replace("{title}", "one"));
+  const search_data = await search_res.text();
+  const search_test = eval(result.search + `search(${JSON.stringify(search_data)})`);
+  if (
+    !search_test[0].hasOwnProperty("id") ||
+    !search_test[0].hasOwnProperty("title") ||
+    !search_test[0].hasOwnProperty("img") ||
+    !search_test[0].hasOwnProperty("extension") ||
+    !search_test[0].hasOwnProperty("authors") ||
+    !search_test[0].hasOwnProperty("artists") ||
+    !search_test[0].hasOwnProperty("description") ||
+    !search_test[0].hasOwnProperty("chapters")
+  ) {
+    console.log("Search test failed; Missing field.");
+    return false;
+  }
+  
+  // Testing if getChapters works
+  const chap_res = await fetch(result.chapters_url.replace("{id}", search_test[0].id));
+  const chap_data = await chap_res.text();
+  const chap_test = eval(result.get_chapters + `getChapters(${JSON.stringify(chap_data)})`);
+  if (
+    !chap_test[0].hasOwnProperty("id") ||
+    !chap_test[0].hasOwnProperty("number") ||
+    !chap_test[0].hasOwnProperty("title") ||
+    !chap_test[0].hasOwnProperty("page") ||
+    !chap_test[0].hasOwnProperty("completed")
+  ) {
+    console.log("Search test failed; Missing field.");
+    return false;
+  }
+
+  // Testing if getChapters works
+  const page_res = await fetch(result.pages_url.replace("{id}", chap_test[0].id));
+  console.log(result.pages_url.replace("{id}", chap_test[0].id));
+  const page_data = await page_res.text();
+  const page_test = eval(result.get_pages + `getChapterPages(${JSON.stringify(page_data)})`);
+  if (page_test.length <= 0) {
+    console.log("Search test failed; Missing field.");
+    return false;
+  }
+  return true;
+}
+
+// Tests plugin, then sends to file
+tests().then((result) => {
+  // Writes to a file, to be inserted as a plugin
+  if (result) {
+    fs.writeFile(`${result.id}.json`, JSON.stringify(result), (error) => {
+        if (error) {
+          console.error(error);
+          throw error;
+        }
+    });
+  }
+})
