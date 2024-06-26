31dc41e5c99bbe972c6bde2f1d7963d46218ec89
Naginipython <42967504+Naginipython@users.noreply.github.com>
2024-06-26 10:21:39 -0400

    Added delete feature

--- frontend/src/routes/settings/+page.svelte
+++ frontend/src/routes/settings/+page.svelte
@@ -1,6 +1,20 @@
 <script>
+    import { invoke } from "@tauri-apps/api/core";
+    import store from "$lib/store.js";
     import Theme from "./theme.svelte";
     import Library from "./library.svelte";
+    async function delete_manga_lib() {
+        invoke('delete_manga_lib');
+        store.update(json => {json.manga_library = []; return json;});
+    }
+    async function delete_ln_lib() {
+        invoke('delete_ln_lib');
+        store.update(json => {json.ln_library = []; return json;});
+    }
+    async function delete_set() {
+        invoke('delete_settings');
+        store.update(json => {json.settings = []; return json;});
+    }
 </script>
 
 <h3 class="set-header">Settings</h3>
@@ -14,11 +28,52 @@
 <h4 class="set-header">Library</h4>
 <Library />
 
+<hr>
+
+<!-- TODO: double check delete -->
+<h4 class="set-header">Data</h4>
+<div class="settings-box">
+    <button class="delete-btn" on:click={async () => delete_manga_lib()}>Delete Manga Library</button>
+</div>
+<div class="settings-box">
+    <button class="delete-btn" on:click={async () => delete_ln_lib()}>Delete LN Library</button>
+</div>
+<div class="settings-box">
+    <button class="delete-btn" on:click={async () => await invoke('delete_manga_plugins')}>Delete Manga Plugins</button>
+</div>
+<div class="settings-box">
+    <button class="delete-btn" on:click={async () => await invoke('delete_ln_plugins')}>Delete LN Library</button>
+</div>
+<div class="settings-box">
+    <button class="delete-btn" on:click={async () => delete_set()}>Delete Settings</button>
+</div>
+
 <style>
     hr {
         border-color: grey;
     }
     .set-header {
         margin-left: 10px;
+    }
+    .settings-box {
+        width: 100vw;
+        overflow: auto;
+        display: inline-flex;
+        background-color: var(--secondary-color);
+    }
+    .settings-box:hover {
+        background-color: var(--selection-color)
+    }
+    .delete-btn {
+        cursor: pointer;
+        display: inline-flex;
+        justify-content: left;
+        align-items: center;
+        border: 0;
+        background-color: transparent;
+        color: inherit;
+        width: 100%;
+        padding: 10px 0;
+        font-size: large;
     }
 </style>
