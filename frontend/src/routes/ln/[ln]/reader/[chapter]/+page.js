195d3228650b52344e045e910ffe98cb3b304849
Naginipython <42967504+Naginipython@users.noreply.github.com>
2024-09-09 08:30:45 -0400

    Deleted useless parts

--- frontend/src/routes/ln/[ln]/reader/[chapter]/+page.js
+++
@@ -1,7 +0,0 @@
-export const prerender = false;
-export function load({ params }) {
-    return {
-        id: params.ln,
-        ln_index: params.chapter
-    }
-}
--- frontend/src/routes/manga/[plugin]/[manga]/reader/[chapter]/+page.js
+++
@@ -1,8 +0,0 @@
-export const prerender = false;
-export function load({ params }) {
-    return {
-        id: params.manga,
-        plugin: params.plugin,
-        manga_index: params.chapter
-    }
-}
